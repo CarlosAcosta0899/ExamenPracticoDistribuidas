217fec99a76f504157d8f26b03cb9d3ae868f449
CarlosAcosta <carlosacosta@upeu.edu.pe>
2022-05-04 19:32:01 -0500

    first commit

---
+++ src/pe/upeu/exaalma/Puerta.java
@@ -0,0 +1,22 @@
+package pe.upeu.exaalma;
+
+public class Puerta {
+    boolean ocupada;
+
+    Puerta(){
+            this.ocupada=false;
+
+    }
+    public boolean estaOcupada(){
+            return this.ocupada;
+    }
+    public synchronized void liberarPuerta(){
+            this.ocupada=false;
+    }
+    public synchronized boolean intentarEntrar(){
+            if (this.ocupada) return false;
+           
+            this.ocupada=true;
+            return true;
+    }
+}
